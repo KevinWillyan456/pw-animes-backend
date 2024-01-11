eec0bf6788f2ee72a0acf40f23b13502ed24b231
Kevin Souza <kevinwillyansouza@gmail.com>
2024-01-11 15:58:12 -0400

    chore: configuração do prettier e dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import express from 'express';
-const app = express();
-const port = 3000;
+import express from 'express'
+import { config } from 'dotenv'
+
+config()
+
+const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+    res.send('Hello World!')
+})
 
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
-});
+    console.log(`Servidor rodando em http://localhost:${port}`)
+})
