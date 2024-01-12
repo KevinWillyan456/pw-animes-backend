3432206b963c0c3aa98b202f33ec9487f8e3d77b
Kevin Souza <kevinwillyansouza@gmail.com>
2024-01-12 18:16:03 -0400

    fix: ativação do cors

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,9 @@ ConnectToDatabase.connect()
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(cors())
 app.use(ApiKeyMiddleware.verifyApiKey)
 
-app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
