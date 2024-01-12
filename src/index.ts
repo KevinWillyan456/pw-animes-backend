0788338914c9a701905222aab89b0952281c26b3
Kevin Souza <kevinwillyansouza@gmail.com>
2024-01-11 21:00:13 -0400

    chore: adição do cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { config } from 'dotenv'
+import cors from 'cors'
 import ConnectToDatabase from './database/MongoDB'
 import { routes } from './routes/routes'
 import ApiKeyMiddleware from './middlewares/ApiKeyMiddleware'
@@ -12,6 +13,7 @@ const port = process.env.PORT || 3000
 
 app.use(ApiKeyMiddleware.verifyApiKey)
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
